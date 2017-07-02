ee17e87624d0fc88b7055e0fbc16466007fbe7c5
bkinsanity <bkinsanity@gmail.com>
2017-07-02 14:12:30 -0700

    Add GET:/logout

--- routes/users.js
+++ routes/users.js
@@ -62,11 +62,16 @@ router.post('/register', upload.single('profileimage'), (req, res, next) => {
       } else {
         console.log(user);
         req.flash('success', 'You are now registered and can login');
-        res.location('/');
-        res.redirect('/');
+        //res.location('/');
+        res.redirect('/login');
       }
     });
   }
 });
 
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success', 'You are now logged out');
+  res.redirect('/users/login');
+});
 module.exports = router;
