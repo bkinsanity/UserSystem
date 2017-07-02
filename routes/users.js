f0b30a9d08da0cc0da7fc8884293fa21449ba3ad
bkinsanity <bkinsanity@gmail.com>
2017-07-02 14:04:13 -0700

    Add user model by mongoose and apply bcryptjs and implement register button

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var multer = require('multer');
 var upload = multer({dest: './uploads'});
+
+var User = require('../models/user');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -45,13 +47,25 @@ router.post('/register', upload.single('profileimage'), (req, res, next) => {
   		errors: errors
   	});
   } else {
-  	/*var newUser = new User({
+  	var newUser = new User({
       name: name,
       email: email,
       username: username,
       password: password,
       profileimage: profileimage
-    });*/
+    });
+    
+    User.createUser(newUser, (err, user) => {
+      if (err) {
+        req.flash('error', err);
+        res.redirect('/users/login');
+      } else {
+        console.log(user);
+        req.flash('success', 'You are now registered and can login');
+        res.location('/');
+        res.redirect('/');
+      }
+    });
   }
 });
 
