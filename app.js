1935f8a4b20f83c9988dbf564536ff27b9c268e0
bkinsanity <bkinsanity@gmail.com>
2017-07-02 15:10:46 -0700

    Add passport module and change the route

--- app.js
+++ app.js
@@ -6,20 +6,21 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var passport = require('passport');
-var expressValidator = require('express-validator');
 var LocalStrategy = require('passport-local').Strategy;
+var expressValidator = require('express-validator');
 var multer = require('multer');
 var upload = multer({dest: './uploads'});
-var flash = require('connect-flash');
+
+var bcrypt = require('bcryptjs');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var flash = require('connect-flash');
 var db = mongoose.connection;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -29,8 +30,6 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Handle Sessions
 app.use(session({
@@ -61,9 +60,17 @@ app.use(expressValidator({
   }
 }));
 
-app.use(require('connect-flash')());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(flash());
 app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+app.get('*', function(req, res, next){
+  res.locals.user = req.user || null;
   next();
 });
 
@@ -100,6 +107,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 var multer = require('multer');
 var upload = multer({dest: './uploads'});
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var User = require('../models/user');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -12,9 +15,51 @@ router.get('/', function(req, res, next) {
 router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Register' });
 });
+
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
+
+router.post('/login',
+  passport.authenticate('local', {failureRedirect:'/users/login', failureFlash: 'Invalid username or password'}),
+  (req, res) => {
+    req.flash('success', 'You are now logged in');
+    res.redirect('/');
+});
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.getUserById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(function (username, password, done) {
+  User.getUserByUsername(username, (err, user) => {
+    if (err) { 
+      throw err;
+    }
+
+    if (!user) {
+      return done(null, false, {message: 'Unknown User'});
+    }
+
+    User.comparePassword(password, user.password, (err, isMatch) => {
+      if (err) {
+        return done(err);
+      }
+
+      if (isMatch) {
+        return done(null, user);
+      } else {
+        return done(null, false, {message:'Invalid Password'});
+      }
+    });
+  });
+}));
 
 router.post('/register', upload.single('profileimage'), (req, res, next) => {
   var name = req.body.name;
@@ -62,7 +107,7 @@ router.post('/register', upload.single('profileimage'), (req, res, next) => {
       } else {
         console.log(user);
         req.flash('success', 'You are now registered and can login');
-        //res.location('/');
+        res.location('/');
         res.redirect('/login');
       }
     });
