2eb12c4579f7ec88d0f52c89b7c2efc5b2fb57ed
bkinsanity <bkinsanity@gmail.com>
2017-07-02 13:47:25 -0700

    Add register form validation

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,58 @@
 var express = require('express');
 var router = express.Router();
-
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register');
+  res.render('register', { title: 'Register' });
+});
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+router.post('/register', upload.single('profileimage'), (req, res, next) => {
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+
+  if (req.file) {
+  	console.log('Uploading File...');
+  	var profileimage = req.file.filename;
+  } else {
+  	console.log('No File Uploaded...');
+  	var profileimage = 'noimage.jpg';
+  }
+
+  // Form Validator
+  req.checkBody('name','Name field is required').notEmpty();
+  req.checkBody('email','Email field is required').notEmpty();
+  req.checkBody('email','Email is not valid').isEmail();
+  req.checkBody('username','Username field is required').notEmpty();
+  req.checkBody('password','Password field is required').notEmpty();
+  req.checkBody('password2','Passwords do not match').equals(req.body.password);
+
+  // Check Errors
+  var errors = req.validationErrors();
+
+  if (errors) {
+  	res.render('register', {
+  		errors: errors
+  	});
+  } else {
+  	/*var newUser = new User({
+      name: name,
+      email: email,
+      username: username,
+      password: password,
+      profileimage: profileimage
+    });*/
+  }
 });
 
 module.exports = router;
