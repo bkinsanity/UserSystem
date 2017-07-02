748ccb826e3c1cb3f3db78ed7cdd7974e04a72ea
bkinsanity <bkinsanity@gmail.com>
2017-07-02 16:14:29 -0700

    Add forgot password feature

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,10 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
 var User = require('../models/user');
+var nodemailer = require('nodemailer');
+var async = require('async');
+var bcrypt = require('bcryptjs');
+crypto = require('crypto');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -119,4 +123,106 @@ router.get('/logout', (req, res) => {
   req.flash('success', 'You are now logged out');
   res.redirect('/users/login');
 });
+
+
+router.get('/forgot', (req, res) => {
+  console.log("I forgot my password");
+  res.render('forgot', {
+    user: req.user
+  });
+});
+
+router.post('/forgot', function(req, res, next) {
+  async.waterfall([
+    function(done) {
+      crypto.randomBytes(20, function(err, buf) {
+        var token = buf.toString('hex');
+        done(err, token);
+      });
+    },
+    function(token, done) {
+      User.findOne({ email: req.body.email }, function(err, user) {
+        if (!user) {
+          req.flash('error', 'No account with that email address exists.');
+          return res.redirect('/forgot');
+        }
+
+        user.resetPasswordToken = token;
+        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+        user.save(function(err) {
+          done(err, token, user);
+        });
+      });
+    },
+    function(token, user, done) {
+      var smtpTransport = nodemailer.createTransport({
+    service: "Gmail",
+    secureConnection: true, // use SSL
+    auth: {
+        user: '',
+        pass: ''
+    }
+});
+
+      var mailOptions = {
+        to: user.email,
+        from: 'bkinsanity@gmail.com',
+        subject: 'Node.js Password Reset',
+        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+          'http://' + req.headers.host + '/users/reset/' + token + '\n\n' +
+          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+      };
+      smtpTransport.sendMail(mailOptions, function(err) {
+        req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+        done(err, 'done');
+      });
+    }
+  ], function(err) {
+    if (err) return next(err);
+    res.redirect('/users/forgot');
+  });
+});
+
+router.get('/reset/:token', function(req, res) {
+  User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+    if (!user) {
+      req.flash('error', 'Password reset token is invalid or has expired.');
+      return res.redirect('/users/forgot');
+    }
+    res.render('reset', {
+      user: req.user
+    });
+  });
+});
+
+router.post('/reset/:token', function(req, res) {
+  async.waterfall([
+    function(done) {
+      User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+        if (!user) {
+          req.flash('error', 'Password reset token is invalid or has expired.');
+          return res.redirect('back');
+        }
+
+        //user.password = req.body.password;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpires = undefined;
+        	
+        bcrypt.genSalt(10, function(err, salt) {
+    	    bcrypt.hash(req.body.password, salt, function(err, hash) {
+   		  	  user.password = hash;
+            user.save(function(err) {
+              done(err, user);
+            });
+    	    });
+	      });
+      });
+    }
+  ], function(err) {
+    res.redirect('/');
+  });
+});
+
 module.exports = router;
