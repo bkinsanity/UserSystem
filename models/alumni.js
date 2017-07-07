0f41e80fc72af80756bd5afd9f854efce2380666
bkinsanity <bkinsanity@gmail.com>
2017-07-07 01:37:18 -0700

    Add pagination feature

--- models/alumni.js
+++ models/alumni.js
@@ -25,13 +25,43 @@ var Alumni = module.exports = mongoose.model('Alumni', AlumniSchema);
 
 module.exports = {
     all: function(req, res){
-        console.log('we are in Alumni.all')
-        Alumni.find({}, function(err, alumnis){
-            if(err) res.render('error', { error: 'Could not fetch items from database :('});
-            console.log('in the callback');
-           // console.log(todos);
-            res.render('alumnis', { alumnis: alumnis });
-        });
+        console.log('we are in Alumni.all');  
+        var perPage = 40
+            , page = req.param('page') > 0 ? req.param('page') : 0;
+
+            res.locals.createPagination = function (pages, page) {
+                var url = require('url')
+                , qs = require('querystring')
+                , params = qs.parse(url.parse(req.url).query)
+                , str = ''
+
+                params.page = 0
+                var clas = "active";
+                str += '<li class="'+clas+'"><a href="?'+qs.stringify(params)+'">第一頁</a></li>';
+                params.page = page == 0 ? 0 : page - 1;
+                str += '<li class="'+clas+'"><a href="?'+qs.stringify(params)+'">上一頁</a></li>';
+                params.page = page == pages - 1 ? pages - 1 : page + 1;
+                str += '<li class="'+clas+'"><a href="?'+qs.stringify(params)+'">下一頁</a></li>';
+                params.page = pages;
+                str += '<li class="'+clas+'"><a href="?'+qs.stringify(params)+'">最後一頁</a></li>'
+
+                return str
+            }
+        Alumni
+            .find()
+            //.select('email')
+            .limit(perPage)
+            .skip(perPage * page)
+            .sort({email: 'asc'})
+            .exec(function (err, alumnis) {
+                Alumni.count().exec(function (err, count) {
+                    res.render('alumnis', {
+                        alumnis: alumnis
+                        , page: page
+                        , pages: count / perPage
+                    })
+                })
+            })
     },
     viewOne: function(req, res){
         Alumni.find({ _id: req.params.id }, function(err, alumni){
@@ -76,6 +106,5 @@ module.exports = {
                 console.log(alumni)
             res.redirect('/alumnis');
         });
-    }
-
+    },
 };
